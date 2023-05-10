0a5a54ebea135bd74c43604f5a66f6252e5ac53d
Javier <jmunoz@algusal.com>
2023-05-10 10:38:46 -0600

    Update from Amplify

--- src/pages/auth/pages/LoginPage/LoginPage.tsx
+++ src/pages/auth/pages/LoginPage/LoginPage.tsx
@@ -1,25 +1,35 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Auth } from '@aws-amplify/auth';
-// import awsExports from '../../../../aws-exports';
+import { Auth, Hub, Logger } from 'aws-amplify';
 
 import './LoginPage.scss';
 
 export const LoginPage = () => {
    const navigate = useNavigate();
 
-   useEffect(() => {
-      checkUser();
-   }, []);
-
    const checkUser = async () => {
       try {
          const user = await Auth.currentAuthenticatedUser();
+         console.log('user result', user);
+         getSession();
+
          if (user) {
             navigate('/');
          }
       } catch (error) {
          console.log('User is not logged in');
+      }
+   };
+
+   const getSession = async () => {
+      try {
+         const session = await Auth.currentSession();
+         console.log(session);
+         console.log(session.getIdToken().getJwtToken()); // ID Token
+         console.log(session.getAccessToken().getJwtToken()); // Access Token
+         console.log(session.getRefreshToken().getToken()); // Refresh Token
+      } catch (error) {
+         console.error('Error getting current user session:', error);
       }
    };
 
@@ -31,6 +41,24 @@ export const LoginPage = () => {
          console.error('Error signing in:', error);
       }
    };
+
+   // Use Effects
+   useEffect(() => {
+      checkUser();
+   }, []);
+
+   useEffect(() => {
+      Hub.listen('auth', (data) => {
+         const { payload } = data;
+         console.log('A new auth event has happened: ', data);
+         if (payload.event === 'signIn') {
+            console.log('a user has signed in!');
+         }
+         if (payload.event === 'signOut') {
+            console.log('a user has signed out!');
+         }
+      });
+   }, []);
 
    return (
       <section className='loginContainer'>
@@ -52,3 +80,72 @@ export const LoginPage = () => {
       </section>
    );
 };
+
+// import { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { Auth } from '@aws-amplify/auth';
+
+// import './LoginPage.scss';
+
+// export const LoginPage = () => {
+//    const navigate = useNavigate();
+
+//    // Anonymous functions
+//    const checkUser = async () => {
+//       try {
+//          const user = await Auth.currentAuthenticatedUser();
+//          if (user) {
+//             navigate('/');
+//          }
+//       } catch (error) {
+//          console.log('User is not logged in');
+//       }
+//    };
+
+//    const getSession = async () => {
+//       try {
+//          const session = await Auth.currentSession();
+//          console.log(session);
+//          console.log(session.getIdToken().getJwtToken()); // ID Token
+//          console.log(session.getAccessToken().getJwtToken()); // Access Token
+//          console.log(session.getRefreshToken().getToken()); // Refresh Token
+//       } catch (error) {
+//          console.error('Error getting current user session:', error);
+//       }
+//    };
+
+//    const handleSignIn = async () => {
+//       try {
+//          const provider = 'Microsoft';
+//          await Auth.federatedSignIn({ customProvider: provider });
+//          await getSession();
+//       } catch (error) {
+//          console.error('Error signing in:', error);
+//       }
+//    };
+
+//    // Use Effects
+//    useEffect(() => {
+//       checkUser();
+//    }, []);
+
+//    return (
+//       <section className='loginContainer'>
+//          <div className='row'>
+//             <div className='loginForm'>
+//                <h3>Welcome!</h3>
+
+//                <p>Please, proceed to login with your Business Microsoft credentials.</p>
+
+//                <div className='login' onClick={handleSignIn}>
+//                   <div className='windowsLogo'>
+//                      <img src={require('../../../../img/WindowsLogo.png')} alt='windows logo' />
+//                   </div>
+
+//                   <h4>Continue with Microsoft</h4>
+//                </div>
+//             </div>
+//          </div>
+//       </section>
+//    );
+// };
