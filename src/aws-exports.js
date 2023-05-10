fa83d1c91fd472c75d03b982f0ca365ce1ec521c
JMSwomie <131408998+JMSwomie@users.noreply.github.com>
2023-05-09 19:21:25 -0600

    Fixed Login

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,8 +3,8 @@
 
 const awsExports = {
     "aws_cognito_identity_pool_id": "us-west-2:c81c9ebb-0de9-46b6-ad52-407db2170d95",
-    "aws_user_pools_id": "us-west-2_IcO8XLoKn",
-    "aws_user_pools_web_client_id": "7454135d2for8o527g4te3fnb5",
+    "aws_user_pools_id": "us-west-2_tnF21pSSA",
+    "aws_user_pools_web_client_id": "6e86t9qc9i2kp48hanna2bfgi8",
     "oauth": {
       "domain": "sync-dev.auth.us-west-2.amazoncognito.com",
       "scope": ["email", "openid"],
@@ -16,44 +16,3 @@ const awsExports = {
   };
   
   export default awsExports;
-
-
-// const awsmobile = {
-//     "aws_project_region": "us-west-2",
-//     "aws_cognito_identity_pool_id": "us-west-2:c81c9ebb-0de9-46b6-ad52-407db2170d95",
-//     "aws_cognito_region": "us-west-2",
-//     "aws_user_pools_id": "us-west-2_IcO8XLoKn",
-//     "aws_user_pools_web_client_id": "7454135d2for8o527g4te3fnb5",
-//     "oauth": {
-//         "domain": "your_cognito_domain",
-//     "scope": ["email", "openid", "profile"],
-//     "redirectSignIn": "your_redirect_signin_url",
-//     "redirectSignOut": "your_redirect_signout_url",
-//     "responseType": "code"
-//     },
-//     "aws_cognito_username_attributes": [
-//         "EMAIL",
-//         "PHONE_NUMBER"
-//     ],
-//     "aws_cognito_social_providers": [],
-//     "aws_cognito_signup_attributes": [
-//         "EMAIL"
-//     ],
-//     "aws_cognito_mfa_configuration": "OFF",
-//     "aws_cognito_mfa_types": [
-//         "SMS"
-//     ],
-//     "aws_cognito_password_protection_settings": {
-//         "passwordPolicyMinLength": 8,
-//         "passwordPolicyCharacters": []
-//     },
-//     "aws_cognito_verification_mechanisms": [
-//         "EMAIL"
-//     ],
-//     "REGION": "us-west-2",
-//     "USER_POOL_ID": "6e86t9qc9i2kp48hanna2bfgi8",
-//     "USER_POOL_APP_CLIENT_ID": "us-west-2_tnF21pSSA"
-// };
-
-
-// export default awsmobile;
--- src/protectedRoute/ProtectedRoute.tsx
+++ src/protectedRoute/ProtectedRoute.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useMsal } from '@azure/msal-react';
+// import { useMsal } from '@azure/msal-react';
 import { MainPage } from '../pages/main/MainPage';
 import { LoginPage } from '../pages/auth';
 
 export const ProtectedRoute = () => {
-   const { accounts } = useMsal();
+   // const { accounts } = useMsal();
    const isAuthenticated = accounts.length > 0;
 
    return isAuthenticated ? <MainPage /> : <LoginPage />;
